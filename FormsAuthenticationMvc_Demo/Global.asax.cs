1ba449b7aedc7f7dabc8989a082f1677b68a9fbc
thetoon30 <thetoon30@gmail.com>
2020-06-27 17:28:11 +0700

    init project

---
+++ FormsAuthenticationMvc_Demo/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using FormsAuthenticationMvc_Demo.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FormsAuthenticationMvc_Demo
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_Error()
+        {
+            Exception exception = Server.GetLastError();
+            //Log your exception here
+            Response.Clear();
+            // clear error on server
+            Server.ClearError();
+
+            Response.Redirect(String.Format("~/Error"));
+        }
+    }
+}
---
+++ FormsAuthenticationMvc_Demo/Models/CustomMembership.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace FormsAuthenticationMvc_Demo.Models
+{
+    public class CustomMembership : MembershipProvider
+    {
+        public override bool EnablePasswordRetrieval => throw new NotImplementedException();
+
+        public override bool EnablePasswordReset => throw new NotImplementedException();
+
+        public override bool RequiresQuestionAndAnswer => throw new NotImplementedException();
+
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override int MaxInvalidPasswordAttempts => throw new NotImplementedException();
+
+        public override int PasswordAttemptWindow => throw new NotImplementedException();
+
+        public override bool RequiresUniqueEmail => throw new NotImplementedException();
+
+        public override MembershipPasswordFormat PasswordFormat => throw new NotImplementedException();
+
+        public override int MinRequiredPasswordLength => throw new NotImplementedException();
+
+        public override int MinRequiredNonAlphanumericCharacters => throw new NotImplementedException();
+
+        public override string PasswordStrengthRegularExpression => throw new NotImplementedException();
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            //ValidatePasswordEventArgs args =
+            //           new ValidatePasswordEventArgs(username, password, true);
+            //OnValidatingPassword(args);
+
+            //if (args.Cancel)
+            //{
+            //    status = MembershipCreateStatus.InvalidPassword;
+            //    return null;
+            //}
+
+            //MembershipUser user = GetUser(username, true);
+
+            //if (user == null)
+            //{
+            //    using (EmployeeContext context = new EmployeeContext())
+            //    {
+            //        var user = 
+            //    }
+            //        UserObj userObj = new UserObj();
+            //    userObj.UserName = username;
+            //    userObj.Password = GetMD5Hash(password);
+            //    userObj.UserEmailAddress = email;
+
+            //    User userRep = new User();
+            //    userRep.RegisterUser(userObj);
+
+            //    status = MembershipCreateStatus.Success;
+
+            //    return GetUser(username, true);
+            //}
+            //else
+            //{
+            //    status = MembershipCreateStatus.DuplicateUserName;
+            //}
+
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(string username, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ValidateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
