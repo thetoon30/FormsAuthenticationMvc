e17171d3c7dc635ae2c2381e44f88d0e07469ed3
thetoon30 <thetoon30@gmail.com>
2020-06-27 17:29:06 +0700

    controller and view

---
+++ FormsAuthenticationMvc_Demo/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormsAuthenticationMvc_Demo.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PaymentController : Controller
+    {
+        // GET: Payment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            int t = Convert.ToInt32("5a55");
+
+            return View();
+        }
+    }
+}
