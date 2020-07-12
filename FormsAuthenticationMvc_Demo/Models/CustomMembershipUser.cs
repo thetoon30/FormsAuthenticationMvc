ffaab85f003e572958b6c3ad25883da3d7599d97
thetoon30 <thetoon30@gmail.com>
2020-07-12 14:44:14 +0700

    class customMemberShipUser

---
+++ FormsAuthenticationMvc_Demo/Models/CustomMembershipUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace FormsAuthenticationMvc_Demo.Models
+{
+    public class CustomMembershipUser : MembershipUser
+    {
+        #region User Properties  
+        public int UserId { get; set; }
+        #endregion
+
+        public CustomMembershipUser(User user) : base("CustomMembership", user.UserName, user.Id, string.Empty, string.Empty, string.Empty, true, false, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now, DateTime.Now)
+        {
+            UserId = user.Id;
+        }
+    }
+}
